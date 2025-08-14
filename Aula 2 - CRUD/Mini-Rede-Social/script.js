64f77bb69d73f090600c57126a20b9337e06cc02
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 21:59:58 -0300

    feat: posting system

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,49 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+const button = document.querySelector("#listar")
+button.addEventListener("click", createPost)
+
+window.onload = listPost()
+
+function listPost(){
+    let postDiv = document.querySelector("#postList")
+    postDiv.innerHTML = " "
+    for (let item in posts){
+        console.log(posts[item].text)
+        let cardPost = document.createElement("div")
+        cardPost.innerHTML = `
+        <h1>${posts[item].text}</h1>
+        <img src = "${posts[item].image}">
+        <p>${posts[item].category}</p>
+        <p>${posts[item].date}</p>
+        `
+
+        postDiv.appendChild(cardPost)
+    
+    }
+}
+
+function createPost(){
+    let post = {
+        text: "",
+        image: "",
+        category: "", 
+        date: ""
+    }
+
+
+    post.text = document.querySelector("#postText").value
+    post.image = document.querySelector("#postImage").value
+    post.category = document.querySelector("#postCategory").value
+    const atualdate = new Date().toLocaleString()
+    post.date = atualdate
+    
+
+    posts.push(post)
+    listPost()
+}
+
+
+
